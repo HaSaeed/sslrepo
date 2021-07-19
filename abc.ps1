84163acdddd7f142ea16bcafe2d38d66b68cea5d
HaSaeed <72789031+HaSaeed@users.noreply.github.com>
2021-07-20 04:52:50 +0500

    Update abc.ps1

--- abc.ps1
+++ abc.ps1
@@ -1,3 +1,3 @@
 Get-Item IIS:\SslBindings\*!443 | Remove-Item
-$CertShop=Get-ChildItem -Path Cert:\LocalMachine\My | where-Object {$_.subject -like "*new*"} | Select-Object -ExpandProperty Thumbprint
-Remove-Item -Path cert:\LocalMachine\My\$certShop
+$CertShop = Get-ChildItem -Path Cert:\LocalMachine\My | where-Object {$_.subject -like "*new*"} | Select-Object -ExpandProperty Thumbprint
+Remove-Item -Path cert:\LocalMachine\My\$CertShop
